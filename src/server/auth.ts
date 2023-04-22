a2a13449c9db018b7b08ee472dd57b37f1fd2986
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2023-04-22 17:22:40 +0200

    refactor: changed import alias

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -4,10 +4,8 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { env } from "~/env.mjs";
-import { prisma } from "~/server/db";
+import { prisma } from "~server/db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -47,12 +45,12 @@ export const authOptions: NextAuthOptions = {
   },
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
     /**
-     * ...add more providers here.
+     * @example provider
+     * DiscordProvider({
+     *  clientId: env.DISCORD_CLIENT_ID,
+     *  clientSecret: env.DISCORD_CLIENT_SECRET,
+     * })
      *
      * Most other providers require a bit more work than the Discord provider. For example, the
      * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
