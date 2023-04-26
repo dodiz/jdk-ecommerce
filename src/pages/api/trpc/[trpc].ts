5672dfc14ffbc5f52d334bc7bc16202e5dad1fae
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2023-04-26 14:49:29 +0200

    fix: imports alias

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,10 +1,9 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
-import { env } from "~/env.mjs";
-import { createTRPCContext } from "~/server/api/trpc";
-import { appRouter } from "~/server/api/root";
+import { env } from "~env";
+import { createTRPCContext } from "~server/api/trpc";
+import { appRouter } from "~server/api/root";
 
-// export API handler
 export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
@@ -12,7 +11,7 @@ export default createNextApiHandler({
     env.NODE_ENV === "development"
       ? ({ path, error }) => {
           console.error(
-            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
           );
         }
       : undefined,
