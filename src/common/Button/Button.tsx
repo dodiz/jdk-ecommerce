22fc5660cf9648d49da19a4d82552257e010b17d
Donato Di Zenzo <donato.dizenzo96@gmail.com>
2023-04-29 19:49:22 +0200

    refactor: removed unused code

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -2,8 +2,6 @@ import styles from "./Button.module.scss";
 
 type ButtonProps = {
   label: string;
-  onClick?: () => void;
-  disabled?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button: React.FC<ButtonProps> = ({ label, ...rest }) => {
